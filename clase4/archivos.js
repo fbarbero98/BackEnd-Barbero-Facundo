ce2d0bb766cf08fb623c0b1f0871cd9ba93ec68e
Facundo Barbero <f.barberops@gmail.com>
2022-07-21 13:05:36 -0300

    Se sumo ejemplo de WFS

--- clase4/archivos.js
+++ clase4/archivos.js
@@ -1,6 +1,8 @@
-//! SEGUNDA PARTE: MANEJO DE ARCHIVOS EN JS
+//! SEGUNDA PARTE: MANEJO DE ARCHIVOS EN JS DE MANERA SINCRONICA
 
 const fs = require('fs'); //Asi es como se importa el modulo nativo de node.
+
+//? readFileSync
 
 const data = fs.readFileSync('./test.txt', 'utf8');  //Defino una constante data, que es igual al uso de la constante fs y el metodo que necesito para leer archivos, en este caso readFileSync.
 
@@ -8,3 +10,15 @@ const data = fs.readFileSync('./test.txt', 'utf8');  //Defino una constante data
 //Tambien se le puede pasar como parametrola codificacion que se usa para la lectura del archivo (igual esta seteado la utf8 por defecto)
 
 console.log(data);
+
+//! PREGUNTAR LO DEL MANEJO DE ARCHIVOS CUANDO ESTOY EN OTRA CARPETA EN NODE
+
+//? YO estaba en la carpeta backend, y cuando hacia el node no me tomaba bien la ruta.
+
+
+//? writeFileSync
+
+fs.writeFileSync('./testWrite.txt', 'Esto es una prueba de de writeFileSync')
+
+//WFS lo que hace es crear un archivo. En el primer parametro creas la ruta al archivo, en el segundo el contenido.
+//Si se vuelve a ejecutar con contenido distinto, se sobreescribe
