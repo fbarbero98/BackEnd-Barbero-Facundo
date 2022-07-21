97e470d553e86182a970ae600dd19908d2ada587
Facundo Barbero <f.barberops@gmail.com>
2022-07-21 11:54:30 -0300

    Clase4

---
+++ clase4/ejercicioClase4.js
@@ -0,0 +1,16 @@
+/*
+function mostrarLetras(params) {
+    const fin = () => console.log('termine')
+    for (let index = 0; index < params.length; index++) {
+        setInterval(() => {
+            console.log([index])
+        }, 0500)
+    }
+    fin()
+}
+
+mostrarLetras('Hola')
+*/
+//Esto hice, esta mal
+
+
