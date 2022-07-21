b9a0fdd560d0461a3ae15bf6c655cc7ebbc96c4f
Facundo Barbero <f.barberops@gmail.com>
2022-07-21 12:58:11 -0300

    Primer ejemplo de manejo de archivos

--- clase4/archivos.js
+++ clase4/archivos.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs'); //Asi es como se importa el modulo nativo de node.
 
-const data = fs.readFileSync('../test.txt', 'utf8');  //Defino una constante data, que es igual al uso de la constante fs y el metodo que necesito para leer archivos, en este caso readFileSync.
+const data = fs.readFileSync('./test.txt', 'utf8');  //Defino una constante data, que es igual al uso de la constante fs y el metodo que necesito para leer archivos, en este caso readFileSync.
 
 //A este metodo hay que pasarle como parametro la ruta del archivo que quiero leer. 
 //Tambien se le puede pasar como parametrola codificacion que se usa para la lectura del archivo (igual esta seteado la utf8 por defecto)
