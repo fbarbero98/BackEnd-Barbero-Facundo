fbffc84e4b9466734ffe090dfbfe2e50306b81a1
Facundo Barbero <f.barberops@gmail.com>
2022-07-21 20:40:04 -0300

    Primer parte del desafio hecha

---
+++ afterclass1/fsPromises.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.promises.readFile('./info.txt', 'utf8')
+    .then(contenido =>{
+        const info = JSON.parse(contenido);
+        console.log(info);
+    })
+
+    
