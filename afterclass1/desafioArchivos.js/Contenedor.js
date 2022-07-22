4693fc7a2f54699c2c0676a9a9e64de09ff5eb6f
Facundo Barbero <f.barberops@gmail.com>
2022-07-21 21:15:29 -0300

    Se hicieron 2 metodos del desafio

--- afterclass1/desafioArchivos.js/Contenedor.js
+++ afterclass1/desafioArchivos.js/Contenedor.js
@@ -1,5 +1,5 @@
 
-const { promises: fs} = require('fs'); //to2 los imports que hagas del node va a tener valor a la hora de ejecutar el archivo.
+const { promises: fs } = require('fs'); //to2 los imports que hagas del node va a tener valor a la hora de ejecutar el archivo.
 
 
 class Contenedor {
@@ -11,16 +11,25 @@ class Contenedor {
     async save(obj) {
 
     }
-    async getbyId(obj) {
-        
+    async getbyId(id) {
+         const products = await this.getAll(); //Obtenemos todos los prods del archivo
+         const productbyId = products.find(prod => prod.id == id); //Buscamos el producto que el id coincida con el que pasamos por param
+         return productbyId; //devuielve el producto
     }
-    async getAll(obj) {
+    async getAll() {
+        try {
+            const products = await fs.readFile(this.ruta, 'utf8'); //Esta trabajando con promesas pero no se escribe por el import
+            console.log('salio bien el getAll')
+            return JSON.parse(products); //Devuelve todos los productos del archivo
+        } catch (error) {
+            console.log(error, 'hubo un error en el getAll')
+            return [];
+        }
+    }
+    async deleteById(id) {
 
     }
-    async deleteById(obj) {
-
-    }
-    async deleteAll(obj) {
+    async deleteAll() {
 
     }
 
