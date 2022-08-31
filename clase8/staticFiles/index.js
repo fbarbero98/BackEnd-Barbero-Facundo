88d8949a257de0d4a00de6760de3ee50bd953d2a
Facundo Barbero <f.barberops@gmail.com>
2022-08-31 20:21:23 -0300

    ?

--- clase8/staticFiles/index.js
+++ clase8/staticFiles/index.js
@@ -8,4 +8,20 @@ app.listen(8080);
 
 app.use(express.static('public')); //Configuramos la aplicacion como un servidor de archivos estaticos.
 
+//Se pueden usar mas de una carpeta. 
+app.use(express.static('files'));
+//Se buscan en orden las carpetas. Si no se encuentra en public, lo busca en files. 
+
+//Tambien se pueden establecer paths virtuales. 
+
+//! PAT VIRTUAL:
+
+app.use('/static' , express.static('public'));
+//Es mejor usar los paths virtuales para exponer la menor cantidad de informacion posible
+
+
+//!PATH ABSOLUTO:
+//No importa desde donde yo quiera correr la aplicacion, si lo hago con path absoluto, nunca nos va a tirar nngun problema.
+
+app.use(express.static(__dirname + 'public'));
 
