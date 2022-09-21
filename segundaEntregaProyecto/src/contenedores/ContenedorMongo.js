85cfe96227159ffa8c92827b785ae223080fc775
Facundo Barbero <f.barberops@gmail.com>
2022-09-21 19:48:46 -0300

    segunda entrega

---
+++ segundaEntregaProyecto/src/contenedores/ContenedorMongo.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+
+const URL = 'mongodb+srv://fbarbero32065:Liceonaval98@cluster0.1dxceik.mongodb.net/test'
+
+
+mongoose.connect(URL, 
+    {useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw new Error(`Error de conexion a la DB ${err}`)
+    console.log('base de datos conectada')
+});
+
+
+class ContenedorMongo{
+    constructor(){
+        this.productos = [],
+        this.id = 1
+    }
+
+    save(obj){
+        try {
+            obj.id = this.id;
+            this.productos = [...this.productos, obj];
+            this.id ++;
+        } catch (error) {
+            console.log('Hubo un error en Save');
+        }
+    }
+    
+     getById(id){
+        try {
+           const producto =  this.getAll();
+           const productsById = producto.find(p => p.id == id);
+           return productsById;  
+        } catch (error) {
+            console.log('Hubo un error en getById')
+        }
+    }
+
+     getAll(){
+        try {
+            console.log(JSON.stringify(this.productos));
+            return JSON.parse(JSON.stringify(this.productos));
+        } catch (error) {
+            console.log('Hubo un error en GetAll');
+        }
+    }
+
+    update(prod, id){
+        try {
+            prod.id = id;
+            this.productos[id - 1] = prod; 
+        } catch (error) {
+           console.log(error) 
+        }
+    }
+
+     deleteById(id){
+        try {
+          const producto =  this.getAll();
+          const productsById = producto.filter(p => p.id != id); 
+          this.productos = productsById;
+        } catch (error) {
+           console.log('Hubo un error en deleteById'); 
+        }
+    }
+    
+}
+
+export default ContenedorMongo;
